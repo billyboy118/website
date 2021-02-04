5fd0a1b2d4366df9d6b64e8220928da76bee3e0f
billyboy118 <bill_matley@hotmail.com>
2021-02-04 18:59:30 +0000

    updates to stuff page

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-var modal1 = document.getElementById("myModal1");
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var btn1 = document.getElementById("myBtn1");
+var btn = document.querySelectorAll("a.modal-button");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
 
 // Get the <span> element that closes the modal
-var span = document.getElementById("c");
-var span1 = document.getElementById("c1");
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-btn1.onclick = function() {
-  modal1.style.display = "block";
-}
+var spans = document.getElementsByClassName("close");
 
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
 }
-span1.onclick = function() {
-  modal1.style.display = "none";
-}
-
-
-
-
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-window.onclick = function(event) {
-  if (event.target == modal1) {
-    modal1.style.display = "none";
-  }
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
 }
